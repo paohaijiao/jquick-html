bd63d6a4f13f4520e3777c8e7887827c24ca9175
martin <goudingcheng@gmail.com>
2025-11-01 15:21:18 +0800

    feature: adjust the element h4 style

---
+++ src/test/java/com/github/paohaijiao/dom/h4/H4Test.java
@@ -0,0 +1,52 @@
+package com.github.paohaijiao.dom.h4;
+
+import com.github.paohaijiao.dom.h3.impl.H3Impl;
+import com.github.paohaijiao.dom.h4.impl.H4Impl;
+import com.github.paohaijiao.dom.p.impl.PImpl;
+import org.junit.Test;
+
+public class H4Test {
+
+    public static H4Impl h1 = new H4Impl();
+
+    @Test
+    public void div() {
+        System.out.println(h1);
+    }
+
+    @Test
+    public void div1() {
+        h1.putAttribute("id","sidebar");
+        System.out.println(h1);
+    }
+
+    @Test
+    public void testAsideWithClassName() {
+        h1.putAttribute("className","sidebar-primary");
+        System.out.println(h1);
+    }
+
+    @Test
+    public void testAsideWithAriaLabel() {
+        h1.putAttribute("AriaLabel","Main sidebar");
+        System.out.println(h1);
+    }
+
+    @Test
+    public void testAsideWithAllAttributes() {
+        h1.putAttribute("id","sidebar");
+        h1.putAttribute("className","sidebar-primary");
+        h1.putAttribute("AriaLabel","Main sidebar");
+        System.out.println(h1);
+    }
+
+    @Test
+    public void testAsideWithTextChild() {
+        h1.putStyle("height","100%");
+        h1.putStyle("width","100%");
+        PImpl paragraph = new PImpl();
+        paragraph.setText("This is a sidebar content");
+        h1.addChild(paragraph);
+        System.out.println(h1);
+    }
+}
